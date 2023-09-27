07a76128117b96c3e65e820f64739d23d2d848a5
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-09-27 06:42:36 -0500

    refactor: :recycle: Refactor file atlas.js
    The code was refactored for better visualization and operation.

--- api/db/atlas.js
+++ api/db/atlas.js
@@ -1,9 +1,12 @@
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 import {MongoClient} from 'mongodb';
-dotenv.config("../");
+// dotenv.config("../");
+import { loadEnv } from "vite";
+const env = loadEnv("development", process.cwd(), 'VITE')
+
 async function con() {
     try {
-        const uri =`mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@alquilercluster.49qmjgl.mongodb.net/${process.env.ATLAS_DB}`
+        const uri =`mongodb+srv://${env.VITE_ATLAS_USER}:${env.VITE_ATLAS_PASSWORD}@alquilercluster.49qmjgl.mongodb.net/${env.VITE_ATLAS_DB}`
         const options ={
             useNewUrlParser:true,
             useUnifiedTopology:true,
@@ -11,7 +14,7 @@ async function con() {
         const cliente =await MongoClient.connect(uri, options);
         return cliente.db();
     } catch (error) {
-        return {status:500, message:error}
+        return {status:500, message:error};
     }
 }
 export {con}
