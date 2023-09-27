514083afbd5ba546c787d3a0734e6efd40909f83
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-09-27 07:07:59 -0500

    feat: :sparkles: Routers, collection to app.js
    Se le agregaron las rutas principales de las collections donde se van hacer las consultas

--- api/app.js
+++ api/app.js
@@ -1,15 +1,15 @@
 import express  from "express";
-import dotenv from "dotenv";
 import cors from "cors";
 import { loadEnv } from "vite";
 import { appToken, appVerify, passport } from "./helpers/token.js";
 import appDiscord from "./routers/discord.js";
+import appIncidents from "./routers/incidents.js";
+import appSupport from "./routers/support.js";
 
 
 const env = loadEnv("development", process.cwd(), 'VITE')
 
 
-dotenv.config();
 
 const app = express();
 app.use(cors());
@@ -19,6 +19,9 @@ app.use(passport.initialize());
 app.use("/login", appToken);
 app.use("/discord", passport.authenticate('bearer', { session: false}) ,appVerify, appDiscord);
 
+app.use("/incidents", passport.authenticate('bearer', { session: false}) ,appVerify, appIncidents);
+
+app.use("/support", passport.authenticate('bearer', { session: false}) ,appVerify, appSupport);
 
 const config={
     port: env.VITE_PORT_BACKEND,
