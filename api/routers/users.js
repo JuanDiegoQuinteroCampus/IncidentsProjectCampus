8bce6b3a6f65fde76170c2f10641dfc51f56e990
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-09-27 09:18:46 -0500

    feat: :sparkles: Add routes, users.js and app.js
    Created the methods to apply to the crud of users.js, thus also creating
    the main routes in app.js.

---
+++ api/routers/users.js
@@ -0,0 +1,37 @@
+import express from "express";
+import { deleteUsers, getAllUsers, postUsers, putUsers } from "../v1/user.js";
+import { proxyUsers, middlewareVerify, DTOData, middlewareParamUsers } from "../middleware/proxyUsers.js";
+import { LimitQuery } from "../helpers/config.js";
+import passportHelper from "../helpers/passportHelper.js"
+
+const appUsers = express();
+appUsers.use(express.json());
+appUsers.use(LimitQuery());
+appUsers.use((req, res, next) => {
+    const apiVersion = req.headers["x-api"];
+    if (apiVersion === "1.0") {
+        next();
+    } else {
+        res.status(400).json({
+            status: 400,
+            message: "API Version No Compatible :("
+        });
+    }
+});
+appUsers.use(passportHelper.authenticate("bearer", {session: false}));
+appUsers.get("/all", middlewareVerify, getAllUsers);
+// appUsers.get("/:id", middlewareVerify, middlewareParamUsers,(req, res, next) => {
+//     const UsersId = req.params.id; 
+//     getAllUsers(req, res, UsersId)
+// });
+appUsers.post("/post", middlewareVerify, proxyUsers , postUsers);
+appUsers.put("/update/:id", middlewareVerify, proxyUsers, async (req, res) => {
+    const UsersId = req.params.id; 
+    putUsers(req, res, UsersId)
+});
+appUsers.delete("/delete/:id", middlewareVerify, async (req, res) => {
+    const UsersId = req.params.id; 
+    deleteUsers(req, res, UsersId)
+});
+
+export default appUsers;
