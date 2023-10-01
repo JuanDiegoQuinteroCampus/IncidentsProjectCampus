850f4439b560951c56b5a7f95dba600f2002dbdc
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-10-01 12:29:08 -0500

    feat: :sparkles: router login and regiter
    se le implementó router para poder manejar de forma sencilla el login y register

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Error404 from './routes/Error404'
+import Register from './routes/Register'
+import Login from './routes/Login'
+
+import FormRegister from './components/FormRegister'
+import FormLogin from './components/FormLogin'
+
+import { NextUIProvider } from "@nextui-org/react" ;   
+
+const root = createBrowserRouter([
+
+  {
+    path:"/",
+    element:<Register/>,
+
+    children:[
+      {
+        path:"Register/",
+        element: <FormRegister/>
+      }
+    ],
+    errorElement: <Error404/>
+  },
+  {
+    path:"/",
+    element:<Login/>,
+
+    children:[
+      {
+        path:"Login/",
+        element: <FormLogin/>
+      }
+    ],
+    errorElement: <Error404/>
+  },
+
+
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <NextUIProvider>
+      <RouterProvider router={root}/>
+    </NextUIProvider>
   </React.StrictMode>,
 )
