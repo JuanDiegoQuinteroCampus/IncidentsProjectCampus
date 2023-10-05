f197c0b1f126a8b55e076bd0a41f5f6a99a09dfe
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-10-05 03:50:35 -0500

    refactor: :recycle: refactor formlogin
    a refactor was made to improve the code as well as the visualization of the form.

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -1,17 +1,17 @@
-import PropTypes from "prop-types";
-import React, { useState, useEffect } from "react";
-import { useOutletContext , useNavigate} from "react-router-dom";
-import {Input, Button, Card, CardBody} from "@nextui-org/react";
+import React, { useState } from 'react';
+import { Input, Button, Card, CardContent } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 export default function FormLogin() {
-  const [nom, setNombre] = useState("");
-  const [password, setPassword] = useState("");
-  const [mensaje, setMensaje] = useState("");
-  const navigate = useNavigate(); 
+  const [nom, setNombre] = useState('');
+  const [password, setPassword] = useState('');
+  const [mensaje, setMensaje] = useState('');
+  const navigate = useNavigate();
 
   const Name = (e) => {
     setNombre(e.target.value);
   };
+
   const contra = (e) => {
     setPassword(e.target.value);
   };
@@ -25,10 +25,10 @@ export default function FormLogin() {
       const response = await fetch(
         `http://${import.meta.env.VITE_HOSTNAME}:${import.meta.env.VITE_PORT_BACKEND}/login`,
         {
-          method: "POST",
+          method: 'POST',
           headers: {
-            "Content-Type": "Application/json",
-            Accept: "Application/json",
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
           },
           body: JSON.stringify(data),
         }
@@ -36,52 +36,81 @@ export default function FormLogin() {
 
       if (response.status === 200) {
         const responseData = await response.json();
-        setMensaje(`Bienvenido, ${data.username}! `);/* Token JWT: ${responseData.jwt} */
+        setMensaje(`Bienvenido, ${data.username}! `); /* Token JWT: ${responseData.jwt} */
         localStorage.setItem('token', responseData.jwt);
         localStorage.setItem('user', data.username);
-        navigate('/')
+        navigate('/');
       } else {
-        setMensaje("Credenciales incorrectas");
+        setMensaje('Credenciales incorrectas');
       }
       console.log(response);
     } catch (error) {
-      console.error("Error al enviar la solicitud:", error);
-      setMensaje("Error interno del servidor");
+      console.error('Error al enviar la solicitud:', error);
+      setMensaje('Error interno del servidor');
     }
 
-    setNombre("");
-    setPassword("");
+    setNombre('');
+    setPassword('');
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault(); // Evitar que el formulario se envíe automáticamente
-    fetchData(); // Llamar a fetchData() cuando se haga clic en "Enviar"
+    e.preventDefault(); 
+    fetchData(); 
   };
 
   return (
-    <>
-    
-    <div className="w-full h-full flex items-center place-content-center flex-col justify-center items-center md:flex-nowrap mb-6 md:mb-0 gap-4 bglogin" style={{ background: 'linear-gradient(to top right, #FF69B4, #FFFF00)', height: '100vh' }}>
-      <Card>
-        <CardBody>
-          <div className="w-full h-full flex flex-col gap-4 bg-white">
-            <h1 className="text-5xl">Login</h1>
+    <div
+      style={{
+        background: 'linear-gradient(to top right, #FF69B4, #FFFF00)',
+        minHeight: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Card style={{ width: '400px' }}>
+        <CardContent>
+          <div className="w-full flex flex-col gap-4 bg-white">
+            <h1 className="text-5xl text-center">Login</h1>
             <form onSubmit={handleSubmit}>
-              <div className="flex w-full flex-col justify-center items-center md:flex-nowrap mb-6 md:mb-0 gap-4">
-                <Input size="lg" label="Username" placeholder="Enter your Username" type="text" id="name" className="w-7/12" onChange={Name} value={nom} />
-                <Input size="lg" type="password" label="Password" placeholder="Enter your password" id="password" className="w-7/12" onChange={contra} value={password} />
-                <Button radius="lg" type="submit" className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg">Send</Button>
+              <div className="flex flex-col gap-4">
+                <Input
+                  size="large"
+                  label="Username"
+                  placeholder="Enter your Username"
+                  type="text"
+                  id="name"
+                  fullWidth
+                  onChange={Name}
+                  value={nom}
+                />
+                <Input
+                  size="large"
+                  type="password"
+                  label="Password"
+                  placeholder="Enter your password"
+                  id="password"
+                  fullWidth
+                  onChange={contra}
+                  value={password}
+                />
+                <Button
+                  variant="contained"
+                  color="primary"
+                  size="large"
+                  type="submit"
+                  fullWidth
+                >
+                  Send
+                </Button>
               </div>
             </form>
             <div>
               <h4>{mensaje}</h4>
             </div>
           </div>
-        </CardBody>
+        </CardContent>
       </Card>
     </div>
-  
-     
-    </>
   );
 }
