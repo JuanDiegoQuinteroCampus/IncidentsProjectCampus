7ec0799eb5dfb4a600129537702d85a316b899a1
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-10-05 03:44:59 -0500

    feat: :sparkles: add content to home
    A content for the home page has been added to the main page.

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,31 +1,33 @@
 import React from "react";
-import img from '../assets/home.jpg'
+import img from '../assets/home.jpg';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 
 const fondo = {
-  backgroundImage: `url(${img})`, // Reemplaza con la ruta de tu imagen
+  backgroundImage: `url(${img})`,
   backgroundSize: "cover",
   backgroundRepeat: "no-repeat",
   backgroundPosition: "center center",
-  height: "90.4vh", // Ajusta la altura como desees
+  minHeight: "100vh", 
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
 };
 
 const contentStyle = {
-  padding: "100px",
+  padding: "6px", 
   borderRadius: "8px",
-  justify: "center",
   display: "flex",
-  flexDirection: "column", 
-  alignItems: "start", 
+  flexDirection: "column",
+  alignItems: "center",
+  maxWidth: "930px", 
+  width: "100%",
 };
 
 const titleStyle = {
-  "fontSize": "3rem",
-  "padding-bottom": "2%"
+  fontSize: "3rem", 
+  paddingBottom: "2%",
 };
 
 const textStyle = {
@@ -35,21 +37,23 @@ const textStyle = {
 export default function Content() {
   return (
     <>
-      <div style={fondo}>
-        <Box sx={{ flexGrow: 1 }}>
-          <Grid container spacing={1}>
-            <Grid xs={9} style={contentStyle}>
-              <h1 className="text-white font-bold text-5xl font-mono mt-4 mb-1" style={titleStyle}>
+    <div style={fondo}>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={9}>
+            <div style={contentStyle}>
+              <Typography variant="h3" component="h1" className="text-white font-bold text-5xl font-mono mt-4 mb-1" style={titleStyle}>
                 Campuslands Incidents
-              </h1>
-              <p className="text-white  text-xl font-sans  mt-2" style={textStyle}>
+              </Typography>
+              <Typography variant="body1" className="text-white text-xl font-sans mt-2" style={textStyle}>
                 In Campusland, we have our own Incident Reporting <br /> system that allows you to report any concerns <br /> quickly and confidentially, helping us <br /> create a safer and more inclusive <br /> university environment.
-              </p>
-            </Grid>
-            <Grid xs={1}></Grid>
+              </Typography>
+            </div>
           </Grid>
-        </Box>
-      </div>
+          <Grid item xs={12} md={3}></Grid>
+        </Grid>
+      </Box>
+    </div>
     </>
   );
 }
